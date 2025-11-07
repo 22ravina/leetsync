dbc32f0e9f912d181e33aa59e3616a0c6f5c695c
RAVINA DHAKA <139635981+22ravina@users.noreply.github.com>
2025-11-07 23:28:55 +0530

    Time: 1 ms (92.97%) | Memory: 61.8 MB (87.62%) - LeetSync

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.c
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+// struct ListNode* modifiedList(int* nums, int numsSize, struct ListNode* head) {
+//     struct ListNode *temp;
+//     struct ListNode *prev=NULL;
+//     for(int i=0;i<numsSize;i++){
+//         temp = head;
+//         prev = NULL;
+//         while(temp!=NULL){
+//             if(nums[i]==temp->val){
+//                 if(temp==head){
+//                     head = temp->next;
+//                 }else{
+//                     prev->next = temp->next;
+//                 }
+//                 temp = temp->next;
+//                 continue;//skip iteration
+//             }
+//             prev = temp;
+//             temp = temp->next;
+            
+//         }
+//     }   
+//     return head; 
+// }
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+#define MAX 100001   // adjust according to constraints
+
+struct ListNode* modifiedList(int* nums, int numsSize, struct ListNode* head) {
+    int mark[MAX] = {0};  // hash-like array for O(1) lookup
+
+    // Step 1: mark all numbers that need to be deleted
+    for (int i = 0; i < numsSize; i++) {
+        mark[nums[i]] = 1;
+    }
+
+    // Step 2: traverse linked list once and delete marked nodes
+    struct ListNode *temp = head;
+    struct ListNode *prev = NULL;
+
+    while (temp != NULL) {
+        if (mark[temp->val]) {  // if this value needs to be deleted
+            if (temp == head) {
+                head = temp->next;
+            } else {
+                prev->next = temp->next;
+            }
+            temp = temp->next;
+            continue;  // skip updating prev when a node is removed
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+
+    return head;
+}
