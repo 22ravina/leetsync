7827b6e6432f1a264bd3105f8cc3441b07ec06a8
RAVINA DHAKA <139635981+22ravina@users.noreply.github.com>
2025-11-30 12:59:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (7.69%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.c
@@ -0,0 +1,32 @@
+bool checkValidString(char* s) {
+    int n = strlen(s);
+    char stack[n];
+    char star[n];
+    int topStar =-1;
+    int top =-1;
+    for(int i = 0; i < n; i++) {
+        if (s[i] == '(') {
+            stack[++top] = i;
+        }
+        else if (s[i] == '*') {
+            star[++topStar] = i;   
+        }else {  // ')'
+            if (top >= 0) { 
+                top--;           // match '('
+            }
+            else if (topStar >= 0) {
+                topStar--;       // use '*' as ')'
+            }
+            else {
+                return false;    // no match available
+            }
+        }
+    }
+    while (top >= 0 && topStar >= 0) {
+        if (stack[top] > star[topStar]) return false;
+        top--;
+        topStar--;
+    }
+
+    return top == -1;
+}
