962ec7b17675797fc0be12d0bf69f0325112d8f1
RAVINA DHAKA <139635981+22ravina@users.noreply.github.com>
2025-11-30 17:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (47.22%) - LeetSync

---
+++ 86-partition-list/partition-list.c
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* partition(struct ListNode* head, int x) {
+    struct ListNode *smallHead = NULL, *smallTail = NULL;
+    struct ListNode *bigHead = NULL, *bigTail = NULL;
+    struct ListNode *curr = head;
+    while (curr != NULL) {
+        if (curr->val < x) {
+            if (smallHead == NULL) {
+                smallHead = curr;
+                smallTail = curr;
+            } else {
+                smallTail->next = curr;
+                smallTail = curr;
+            }
+        } else {
+            if (bigHead == NULL) {
+                bigHead = curr;
+                bigTail = curr;
+            } else {
+                bigTail->next = curr;
+                bigTail = curr;
+            }
+        }
+        curr = curr->next;
+    }
+    if (smallHead == NULL) {
+        if (bigTail) bigTail->next = NULL;
+        return bigHead;
+    }
+    smallTail->next = bigHead;
+
+    if (bigTail) bigTail->next = NULL;
+
+    return smallHead;
+ 
+}
