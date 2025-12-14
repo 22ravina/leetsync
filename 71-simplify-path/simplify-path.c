2a6be81cf4cd8584b75891e63948a9777fda0087
RAVINA DHAKA <139635981+22ravina@users.noreply.github.com>
2025-12-14 18:25:06 +0530

    Time: 1 ms (52.26%) | Memory: 9.5 MB (28.28%) - LeetSync

---
+++ 71-simplify-path/simplify-path.c
@@ -0,0 +1,70 @@
+typedef struct Node{
+    char *data;
+    struct Node *next;
+}Node;
+
+Node *push_st(Node *head,char *val){
+    if(val==NULL){
+        return head;
+    }
+    Node *newNode = malloc(sizeof(Node));
+    int n = strlen(val);
+    newNode->data = malloc((n+1)*sizeof(char));
+    strcpy(newNode->data,val);
+
+    newNode->next = head;
+    return newNode;
+}
+
+Node *pop_st(Node *head){
+    if (head == NULL) {
+        return NULL;
+    }
+    Node *temp = head;
+    Node* newHead = head->next;
+    free(temp->data);
+    free(temp);
+    return newHead;
+}
+char* simplifyPath(char* path) {
+    Node *head = NULL;
+    int n = strlen(path);
+
+    char *token = strtok(path,"/");
+    while(token!=NULL){
+        if (strcmp(token, ".") == 0) {
+            // do nothing
+        }
+        else if (strcmp(token, "..") == 0) {
+            head = pop_st(head);
+        }
+        else {
+            head = push_st(head, token);
+        }
+
+        token = strtok(NULL,"/");
+    }
+
+    Node* rev = NULL;
+    while (head != NULL) {
+        rev = push_st(rev, head->data);
+        head = pop_st(head);
+    }
+    char *ans = malloc(n+ 2);
+    int i = 0;
+    ans[i++] = '/';
+
+    while (rev != NULL) {
+        int len = strlen(rev->data);
+        memcpy(ans + i, rev->data, len);
+        i += len;
+
+        if (rev->next != NULL)
+            ans[i++] = '/';
+
+        rev = rev->next;
+    }
+    ans[i] = '\0';
+    return ans;
+
+}
