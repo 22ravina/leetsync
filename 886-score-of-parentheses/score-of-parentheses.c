a0171f2d6145dfde271fa8a27b06f86a003bc2d1
RAVINA DHAKA <139635981+22ravina@users.noreply.github.com>
2025-12-17 19:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (96.92%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.c
@@ -0,0 +1,16 @@
+int scoreOfParentheses(char* s) {
+    int score = 0;
+    int depth = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '(') {
+            depth++;
+        } else {
+            depth--;
+            if (s[i-1] == '(') {
+                score += 1 << depth;   
+            }
+        }
+    }
+    return score;
+}
